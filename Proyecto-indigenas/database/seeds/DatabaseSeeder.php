66681fc58ffcd12e7745853614a47513efcb04c5
Roger Rodriguez <alejoicecream@gmail.com>
2022-08-12 11:16:43 -0500

    s

--- Proyecto-indigenas/database/seeds/DatabaseSeeder.php
+++ Proyecto-indigenas/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // php artisan db:seed
         $this->call([
             TipoDocumentoSeeder::class,
             RolSeeder::class,
