ced1a59725141a99f7286fdd31e3074367a4a211
Roger Rodriguez <alejoicecream@gmail.com>
2022-08-17 18:23:54 -0500

    Add new filtro opcion reporte models

---
+++ Proyecto-indigenas/app/Model/filtro_opcion.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class filtro extends Model
+{
+    //
+    protected $connection = 'mysql';
+
+    protected $table = 'filtro_opcion';
+
+    public $timestamps = false;
+
+    protected $primaryKey = 'id_filtro_opcion';
+
+    protected $fillable = [
+        'fk_id_filtro',
+        'fk_id_opcion',
+        'fecha_creacion',
+        'fk_id_usuario'
+    ];
+
+    public function filtro()
+    {
+        return $this->belongsTo('App\Model\filtro', 'fk_id_filtro', 'id_filtro');
+    }
+
+    public function opcion()
+    {
+        return $this->belongsTo('App\Model\opcion', 'fk_id_opcion', 'id_opcion');
+    }
+  
+}
---
+++ Proyecto-indigenas/app/Model/reporte_filtro_opcion.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class filtro extends Model
+{
+    //
+    protected $connection = 'mysql';
+
+    protected $table = 'reporte_filtro_opcion';
+
+    public $timestamps = false;
+
+    protected $primaryKey = 'id_reporte_filtro_opcion';
+
+    protected $fillable = [
+        'fk_id_filtro_opcion',
+        'fk_id_reporte',
+        'fecha_creacion',
+        'fk_id_usuario'
+    ];
+
+    public function filtro_opcion()
+    {
+        return $this->belongsTo('App\Model\filtro_opcion', 'fk_id_filtro_opcion', 'id_filtro_opcion');
+    }
+
+    public function reporte()
+    {
+        return $this->belongsTo('App\Model\reporte', 'fk_id_reporte', 'id_reporte');
+    }
+  
+}
