3bdc6fa0bdf1fe4380056b7c2076dc8588a29535
Roger Rodriguez <alejoicecream@gmail.com>
2022-10-25 23:37:36 -0500

    capturar click dentro de grafica

--- Proyecto-indigenas/public/assets/js/reporte.js
+++ Proyecto-indigenas/public/assets/js/reporte.js
@@ -1,10 +1,11 @@
 
 
 let myBubbleChart;
+let data;
 
 const renderChart = datosGrafica => {
 
-    const data = {
+    data = {
         labels: [...new Set(datosGrafica.map(d => d.source))],
         datasets: [{
             data: datosGrafica.map(d => d.valor),
@@ -17,14 +18,24 @@ const renderChart = datosGrafica => {
     const options = {
         plugins: {
             legend: { position: 'left' }
-        }
+        },
+        onClick: graphClickEvent
     }
-    
-
     if(myBubbleChart instanceof Chart)
     {
         myBubbleChart.destroy();
     }
     
-    myBubbleChart  = new Chart('modelsChart', { type: 'doughnut', data, options })
+    myBubbleChart = new Chart('modelsChart', { type: 'doughnut', data, options })
+}
+
+function graphClickEvent(e, activeEls){
+    
+    let datasetIndex = activeEls[0].datasetIndex;
+    let dataIndex = activeEls[0].index;
+    
+    let value = data.datasets[0].data[dataIndex];
+    let label = data.labels[dataIndex];
+    alert("OPCION: " + label + " PORCENTAJE: " + value);
+
 }
